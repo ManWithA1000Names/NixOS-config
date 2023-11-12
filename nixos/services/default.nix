44696b63faf64e48156a2e23aa2a6bf2a388f8bb
ManWithA1000Names <loena.phivos2010@gmail.com>
2023-11-12 18:34:07 +0200

    avahi stuff

--- nixos/services/default.nix
+++ nixos/services/default.nix
@@ -3,7 +3,10 @@
     printing.enable = true;
     resolved.enable = true;
     gvfs.enable = true;
-    avahi.enable = true;
+    avahi = {
+      enable = true;
+      nssmdns = true;
+    };
     openssh = {
       enable = true;
       settings.PermitRootLogin = "no";
