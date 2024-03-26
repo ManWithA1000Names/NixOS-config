6b51911aceabbf86962edbeb3aa88b2ac7d45bd2
ManWithA1000Names <loena.phivos2010@gmail.com>
2024-03-26 20:24:29 +0200

    add ntfs support

--- nixos/boot/default.nix
+++ nixos/boot/default.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "ntfs" ];
   systemd.watchdog.rebootTime = "0";
 
   i18n.defaultLocale = "en_US.UTF-8";
