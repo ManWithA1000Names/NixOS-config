ea0506a056be844d960ea8cacc52ef05dfb9d2e2
ManWithA1000Names <loena.phivos2010@gmail.com>
2024-10-19 09:44:28 +0300

    fix previous commit

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -23,7 +23,7 @@
   time.timeZone = "Europe/Athens";
   i18n = {
     defaultLocale = "en_US.UTF-8";
-    supportedLocales = [ "en_US.UTF-8" "el_GR.UTF-8" ];
+    supportedLocales = [ "en_US.UTF-8/UTF-8" "el_GR.UTF-8/UTF-8" ];
   };
 
   # nix specifics
