341c012ecf99307dcdae026b5c099a2f4d7dc078
ManWithA1000Names <loena.phivos2010@gmail.com>
2025-01-06 06:12:19 +0200

    fix: ssh, stop suspend on lidSwitch close

--- nixos/environment/user.nix
+++ nixos/environment/user.nix
@@ -2,18 +2,16 @@
   users.users.user = {
     isNormalUser = true;
     description = "The human user";
+
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKvGywW2M93I8qbcmQanE9GAEggfUGiwLCP3fAPip6mV user@big-boss"
+    ];
+
     extraGroups =
       [ "networkmanager" "wheel" "kvm" "input" "libvirtd" "docker" ];
+
     shell = pkgs.fish;
-    packages = with pkgs; [
-      fd
-      bat
-      eza
-      aria
-      file
-      peco
-      ripgrep
-      starship
-    ];
+
+    packages = with pkgs; [ fd bat eza aria file peco ripgrep starship ];
   };
 }
--- nixos/services/default.nix
+++ nixos/services/default.nix
@@ -1,10 +1,17 @@
-{ pkgs, ... }: {
+_: {
   services = {
     openssh = {
       enable = true;
-      settings.PermitRootLogin = "no";
-      knownHosts.big-boss.publicKey =
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKvGywW2M93I8qbcmQanE9GAEggfUGiwLCP3fAPip6mV user@big-boss";
+      settings = {
+        PermitRootLogin = "no";
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+      };
+    };
+
+    logind = {
+      lidSwitch = "ignore";
+      lidSwitchDocked = "ignore";
     };
   };
 }
