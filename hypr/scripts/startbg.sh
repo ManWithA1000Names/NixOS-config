f0ecfef97ae2680b7ca2778b20e0c18f1578cc1c
ManWithA1000Names <loena.phivos2010@gmail.com>
2025-01-07 22:57:15 +0200

    chagne background for vertical monitor

--- hypr/scripts/startbg.sh
+++ hypr/scripts/startbg.sh
@@ -2,4 +2,6 @@
 
 PAPER="$(find /home/user/.config/hypr/Wallpapers -type f | shuf | head -1)"
 sed "s@\$paper@$PAPER@g" /home/user/.config/hypr/hyprpaper.conf >/tmp/hypr/hyprpaper.conf
+PAPER_SPECIAL="$(find /home/user/Pictures/DC_Characters/ -type f | shuf | head -1)"
+sed -i "s@\$special@$PAPER_SPECIAL@g" /tmp/hypr/hyprpaper.conf
 hyprpaper -c /tmp/hypr/hyprpaper.conf
