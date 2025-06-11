0e6581cd3c3238ba4408eb204fa68985b36910d4
ManWithA1000Names <loena.phivos2010@gmail.com>
2025-06-11 19:46:22 +0300

    make lock screen use same background

--- hypr/scripts/startbg.sh
+++ hypr/scripts/startbg.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-PAPER="$(find /home/user/.config/hypr/Wallpapers -type f | shuf | head -1)"
-sed "s@\$paper@$PAPER@g" /home/user/.config/hypr/hyprpaper.conf >/tmp/hypr/hyprpaper.conf
-PAPER_SPECIAL="$(find /home/user/Pictures/DC_Characters/fixed/ -type f | shuf | head -1)"
+mkdir -p /tmp/hypr/
+
+PAPER="$(find ~/.config/hypr/Wallpapers -type f | shuf | head -1)"
+sed "s@\$paper@$PAPER@g" ~/.config/hypr/hyprpaper.conf >/tmp/hypr/hyprpaper.conf
+sed "s@\$paper@$PAPER@g" ~/.config/hypr/hyprlock.conf >/tmp/hypr/hyprlock.conf
+PAPER_SPECIAL="$(find ~/Pictures/DC_Characters/fixed/ -type f | shuf | head -1)"
 sed -i "s@\$special@$PAPER_SPECIAL@g" /tmp/hypr/hyprpaper.conf
+sed -i "s@\$special@$PAPER_SPECIAL@g" /tmp/hypr/hyprlock.conf
 hyprpaper -c /tmp/hypr/hyprpaper.conf
