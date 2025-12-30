b2d114c74dfecec6ad2bbfb57f5966fc6325a5d1
ManWithA1000Names <loena.phivos2010@gmail.com>
2025-12-30 22:26:33 +0200

    fix

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -10,6 +10,7 @@
     [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelParams = [ "fsck.mode=force" "fsck.repair=yes" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
