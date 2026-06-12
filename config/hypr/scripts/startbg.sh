2a24eda3f522f5a636044315036012213f247cf0
ManWithA1000Names <loena.phivos2010@gmail.com>
2026-06-12 09:29:51 +0300

    add shuffling of wallpapers for the special wallpapers

--- config/hypr/scripts/startbg.sh
+++ config/hypr/scripts/startbg.sh
@@ -6,6 +6,5 @@ PAPER="$(find ~/.config/hypr/Wallpapers -type f | shuf | head -1)"
 sed "s@\$paper@$PAPER@g" ~/.config/hypr/hyprpaper.conf >/tmp/hypr/hyprpaper.conf
 sed "s@\$paper@$PAPER@g" ~/.config/hypr/hyprlock.conf >/tmp/hypr/hyprlock.conf
 PAPER_SPECIAL="$(find ~/Pictures/DC_Characters/fixed/ -type f | shuf | head -1)"
-sed -i "s@\$special@$PAPER_SPECIAL@g" /tmp/hypr/hyprpaper.conf
 sed -i "s@\$special@$PAPER_SPECIAL@g" /tmp/hypr/hyprlock.conf
 hyprpaper -c /tmp/hypr/hyprpaper.conf
